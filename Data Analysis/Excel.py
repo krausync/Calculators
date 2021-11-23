4127bc9fc0eb0a50c706892e48342bb694abe834
thiagokraus <thiagokraussilva@hotmail.com>
2021-11-23 20:37:55 -0300

    1.0 Version

---
+++ Data Analysis/Excel.py
@@ -0,0 +1,8 @@
+# Programa que irá solicitar o path, nome do arquivo e extensão e então fará a leitura.
+import pandas as pd
+A = input("Insira aqui o Path = ")
+B = input("Insira aqui o nome do arquivo = ")
+C = input("Insira a extensão do arquivo = ")
+caminho = A + "/" + B + "." + C;
+teste = pd.read_excel(caminho)
+print(teste)
